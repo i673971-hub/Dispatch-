7cef4e0a953dfc7aa23a92def2b392f29aa6c6bb
Pame <i673971@gmail.com>
2026-09-03 18:21:44 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Dispatch from "./Dispatch.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Dispatch />
+  </React.StrictMode>
+);
